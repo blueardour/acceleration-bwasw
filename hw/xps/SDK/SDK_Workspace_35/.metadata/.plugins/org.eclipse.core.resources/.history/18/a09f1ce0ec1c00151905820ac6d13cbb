ff3ee834d237778e537a88b610f128baeffe5034
blueardour <blueardour@gmail.com>
2015-06-27 11:22:53 -0700

    sdk and shot

---
+++ hw/xps/SDK/SDK_Workspace_35/.metadata/.plugins/org.eclipse.core.resources/.history/18/a09f1ce0ec1c00151905820ac6d13cbb
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/platform.c 
+
+OBJS += \
+./src/main.o \
+./src/platform.o 
+
+C_DEPS += \
+./src/main.d \
+./src/platform.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze gcc compiler'
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../standalone_bsp_0/microblaze_0/include -mxl-barrel-shift -mxl-pattern-compare -mcpu=v8.00.b -mno-xl-soft-mul -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ hw/xps/SDK/SDK_Workspace_35/hello_world_0/Debug/src/main.d
@@ -0,0 +1,42 @@
+src/main.d src/main.o: ../src/main.c \
+  ../../standalone_bsp_0/microblaze_0/include/xintc.h \
+  ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+  ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+  ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+  ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+  ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+  ../../standalone_bsp_0/microblaze_0/include/xintc_l.h \
+  ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+  ../../standalone_bsp_0/microblaze_0/include/sysace_stdio.h \
+  ../../standalone_bsp_0/microblaze_0/include/fsl.h \
+  ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+  ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+  ../src/platform.h ../src/platform_config.h
+
+../../standalone_bsp_0/microblaze_0/include/xintc.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/sysace_stdio.h:
+
+../../standalone_bsp_0/microblaze_0/include/fsl.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
